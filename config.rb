fb8cf16ce960ba22672bc525c2abecd585185a5a
BRUNA <brutiko@gmail.com>
2017-02-11 02:11:26 +0100

    Fix pages.

--- config.rb
+++ config.rb
@@ -12,6 +12,7 @@ page '/*.txt', layout: false
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 page "/conceito/*", :layout => "article"
+page "/referencia.html", :layout => "article"
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
